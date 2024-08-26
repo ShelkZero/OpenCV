60b008c50d328d19c71113d9a193a4cab6a1cbc5
Anna Karban <annakant37@gmail.com>
2024-08-26 16:16:06 +0300

    add program

--- OpenCV/OpenCV.py
+++ OpenCV/OpenCV.py
@@ -10,6 +10,7 @@ photo = np.zeros((450, 450, 3), dtype='uint8')
 #cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2), (119, 201, 105), thickness=3)
 
 #cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 100, (181, 176, 38), thickness=cv2.FILLED)
+#Привет)))))))))
 
 cv2.putText(photo, "Shelk", (100, 150), cv2.FONT_HERSHEY_TRIPLEX, 1, (181, 176, 38), 2)
 cv2.imshow('Photo', photo)
